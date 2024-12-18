496930c23c9c56e4011eaf96a99d6cb17212011a
张杰诚 <2234829205@qq.com>
2024-12-18 16:22:06 +0800

    修改包名

---
+++ src/main/java/com/muyu/springbootinit/mapper/PostThumbMapper.java
@@ -0,0 +1,18 @@
+package com.muyu.springbootinit.mapper;
+
+import com.muyu.springbootinit.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ *
+ *   
+ *  
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
---
+++ src/main/java/com/muyu/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.muyu.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.muyu.springbootinit.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ *   
+ *  
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
